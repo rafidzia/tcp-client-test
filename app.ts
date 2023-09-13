3438df21dfc6c62fdf9a2afd05acf09f1e3dffc9
FaridAA <faridabdul1406@gmail.com>
2023-09-13 17:32:34 +0700

    define host

--- app.ts
+++ app.ts
@@ -2,7 +2,7 @@ import net from "node:net"
 import test from "./test"
 
 function run(){
-  const client = net.createConnection({ port: 8080 }, () => {
+  const client = net.createConnection({ host: "127.0.0.1", port: 8080 }, () => {
       console.log("connected to server")
       test(client) 
       setInterval(()=>{
