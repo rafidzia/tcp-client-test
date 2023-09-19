5ce45827f145e8a5d5e1a616467ea90cac3bce84
FaridAA <faridabdul1406@gmail.com>
2023-09-20 00:38:37 +0700

    add interval remover

--- client.ts
+++ client.ts
@@ -2,10 +2,11 @@ import {createConnection} from "net"
 import test from "./test"
 
 function run(){
+  let testInterval: NodeJS.Timeout | null
   const client = createConnection({ host: "127.0.0.1", port: 2000 }, () => {
       // console.log("connected to server")
       test(client) 
-      setInterval(()=>{
+      testInterval = setInterval(()=>{
         test(client) 
       }, 5000)
   })
@@ -23,10 +24,12 @@ function run(){
   // })
 
   setTimeout(()=>{
+    clearInterval(testInterval!)
     client.destroy()
     setTimeout(()=>{
       // @ts-ignore
       client = null
+      testInterval = null
     }, 1000)
   }, 600000)
 }
