c0116f17021d86fab6d21744eba35e002e27ce06
rafidzia <faridabdul1406@gmail.com>
2023-10-27 13:54:51 +0700

    yes

--- test3.ts
+++ test3.ts
@@ -12,18 +12,18 @@ let countClient = 0
 
 function run() {
     let testInterval: NodeJS.Timeout | null
-    let imei: number
-    if(recycledImei.length > 0){
-        let tmpimei = recycledImei.pop()
-        if(tmpimei){
-            imei = tmpimei
-        }else{
-            imei = Math.floor(100000000000000 + Math.random() * 900000000000000)
-        }
-    }else{
-        imei = Math.floor(100000000000000 + Math.random() * 900000000000000)
-    }
-    let client = createConnection({ host: "127.0.0.1", port: 2000 }, () => {
+    let imei: number = 375986606241231
+    // if(recycledImei.length > 0){
+    //     let tmpimei = recycledImei.pop()
+    //     if(tmpimei){
+    //         imei = tmpimei
+    //     }else{
+    //         imei = Math.floor(100000000000000 + Math.random() * 900000000000000)
+    //     }
+    // }else{
+    //     imei = Math.floor(100000000000000 + Math.random() * 900000000000000)
+    // }
+    let client = createConnection({ host: "localhost", port: 1200 }, () => {
         countClient++
         // console.log("connected to server")
         client.write(Buffer.from("787811010" + imei.toString() + "20082bc10bec39760d0a", "hex"))
@@ -38,7 +38,8 @@ function run() {
         if (data.toString("hex") == "7878050110014c4d0d0a") {
             client.write(Buffer.from("78780a1346060400020bef60b70d0a", "hex"))
         }
-        if (data.toString("hex") == "787812800c0000000056455253494f4e231011e1b50d0a787813800b0000000056455253494f4e00021011da780d0a") {
+        if(data.toString("hex").includes("787813800b0000000056455253494f4e00021011da780d0a") || data.toString("hex").includes("787812800c0000000056455253494f4e231011e1b50d0a")){
+            console.log("asd")
             client.write(Buffer.from("797900332100000000015b56455253494f4e5d4e5433375f47543831305f574141445f56332e315f3232303930372e313631380bed993e0d0a", "hex"))
         }
     });
@@ -56,12 +57,10 @@ function run() {
     }, 1200000)
 }
 
-run()
-
 // setInterval(() => {
 //     console.log("client count: ", countClient)
 // }, 10000)
 
 // setInterval(() => {
-//     run()
+    run()
 // }, 100)
